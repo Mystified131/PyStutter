7739c3103525fa1a15e0a658328ce26935278362
Thomas Park <mystifiedthomas@gmail.com>
2022-07-06 18:12:23 -0500

    tweaking

---
+++ SoundGenGo.py
@@ -0,0 +1,15 @@
+#This code imports the necessary modules.
+
+import os
+from subprocess import call
+
+
+print("")
+
+print("Initiating Sequence.")
+
+print("")
+
+call(["python", "BatchEthnoSoundsStut.py"])
+
+## THE GHOST OF THE SHADOW ##
